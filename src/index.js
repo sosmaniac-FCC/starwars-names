8e2b57ce26a51e66f53ec835833bb603c11e2a46
John Simmons <jgsimmons42@students.tntech.edu>
2018-05-02 13:40:57 -0500

    feat(index):
    namesindexd
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const uniqueRandomArray = require('unique-random-array');
 const starWarsNames = require('./starwars-names.json');
+const getRandomItem = uniqueRandomArray(starWarsNames);
 
 module.exports = {
   all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames)
+  random: random
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  }
+  var randomItems = [];
+  for (var i = 0; i < number; i++) {
+    randomItems.push(getRandomItem());
+  }
+  return randomItems;
+}
